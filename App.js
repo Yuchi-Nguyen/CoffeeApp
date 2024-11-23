2dc72060df6fc34cf7ac38808e319428454b6ae0
Nguyen Thanh Tinh <ngthanhtinh801@gmail.com>
2024-11-23 10:09:42 +0700

    add header info

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { AuthProvider } from './context/AuthContext';
 import RootNavigator from './Navigation/RootNavigator';
+import Header from './Components/UserInfoHeader';
 export default function App() {
   return (
-    <AuthProvider>
-      <RootNavigator />
-    </AuthProvider>
+    // <AuthProvider>
+    //   <RootNavigator />
+    // </AuthProvider>
+    <Header
+      userName="Uchiha Obito"
+      memberLevel="MEMBER"
+      drips={120}
+      prepaid={5000}
+    />
   );
 }
