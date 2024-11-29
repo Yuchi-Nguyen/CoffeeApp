77ab6fdb456eede162bbc8f5a5ed813adfe6ae9b
TrisNg <nhtri21131@cusc.ctu.edu.vn>
2024-11-29 21:36:34 +0700

    Update OthersPage

--- Pages/OthersPage.js
+++ Pages/OthersPage.js
@@ -19,6 +19,7 @@ const OthersPage = () => {
   const [disableDatePicker, setDisableDatePicker] = useState(true);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [profile, setProfile] = useState({
+    displayName: '',
     firstName: '',
     lastName: '',
     gender: '',
@@ -41,6 +42,7 @@ const OthersPage = () => {
           if (userDoc.exists()) {
             const userData = userDoc.data();
             setProfile({
+              displayName: userData.displayName || '',
               firstName: userData.firstName || '',
               lastName: userData.lastName || '',
               gender: userData.gender || '',
@@ -116,7 +118,7 @@ const OthersPage = () => {
   return (
     <>
       <Header
-        userName={`${profile.firstName} ${profile.lastName}`}
+        userName={profile.displayName || `${profile.firstName} ${profile.lastName}`}
         memberLevel="MEMBER"
         drips={profile.drips}
         prepaid={profile.prepaid}
