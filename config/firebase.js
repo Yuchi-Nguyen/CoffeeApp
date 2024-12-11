a0363d5f4916abf033f93f70e34cc59b85a5c408
TrisNg <nhtri21131@cusc.ctu.edu.vn>
2024-12-11 17:23:51 +0700

    All file re-up

--- config/firebase.js
+++ config/firebase.js
@@ -1,31 +1,24 @@
 import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID,
-} from '@env';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID
+  apiKey: "AIzaSyAL45M5wAW7lmH1ZntiL2GDseoEkJM4rXU",
+  authDomain: "coffeeapp-77dae.firebaseapp.com",
+  projectId: "coffeeapp-77dae",
+  storageBucket: "coffeeapp-77dae.firebasestorage.app",
+  messagingSenderId: "888337175348",
+  appId: "1:888337175348:web:f6d3caffbe1945fbf0cb61",
+  measurementId: "G-B2LZ71BH9S"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Khởi tạo Auth với AsyncStorage
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
-});
-
+// Initialize services
 export const db = getFirestore(app);
-export { auth };
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
+export default app;
